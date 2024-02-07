0f449cc427e1440a2a311f59c4bf61f3c4c2cb7c
Jhoe Hashimoto <147004858+JhoeHashimoto@users.noreply.github.com>
2024-02-07 17:52:44 -0300

    Create ex50.py

---
+++ ex50.py
@@ -0,0 +1,2 @@
+
+##50. Entrar via teclado com doze valores e armazená-los em uma matriz de ordem 3x4. Após a digitação dos valores solicitar uma constante multiplicativa, que deverá multiplicar cada valor matriz e armazenar o resultado na própria matriz, nas posições correspondentes.
